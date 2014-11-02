99a5930696d13f254b99283ad5d1477d4ecb73e1
Sonal Saldanha <sonal.saldanha@gmail.com>
2014-11-02 22:37:52 +0530

    kmp update

--- Algorithms/KnuthMorrisPratt.py
+++ Algorithms/KnuthMorrisPratt.py
@@ -1,32 +1,34 @@
-class KnuthMorrisPratt(StringSearcher):
-    def search(self, text, pattern):
-        """Yields all starting positions of copies of the pattern in the text.
+
+
+class KnuthMorrisPratt(object):
+    def search(self, string, substring):
+        """Yields all starting positions of copies of the substring in the string.
   Calling conventions are similar to string.find, but its arguments can be
   lists or iterators, not just strings, it returns all matches, not just
-  the first one, and it does not need the whole text in memory at once.
-  Whenever it yields, it will have read the text exactly up to and including
+  the first one, and it does not need the whole string in memory at once.
+  Whenever it yields, it will have read the string exactly up to and including
   the match that caused the yield."""
 
-        # allow indexing into pattern and protect against change during yield
-        pattern = list(pattern)
+        # allow indexing into substring and protect against change during yield
+        substring = list(substring)
 
         # build table of shift amounts
-        shifts = [1] * (len(pattern) + 1)
+        shifts = [1] * (len(substring) + 1)
         shift = 1
-        for pos in range(len(pattern)):
-            while shift <= pos and pattern[pos] != pattern[pos - shift]:
+        for pos in range(len(substring)):
+            while shift <= pos and substring[pos] != substring[pos - shift]:
                 shift += shifts[pos - shift]
             shifts[pos + 1] = shift
 
         # do the actual search
         start_pos = 0
         match_len = 0
-        for c in text:
-            while match_len == len(pattern) or match_len >= 0 and pattern[match_len] != c:
+        for c in string:
+            while match_len == len(substring) or match_len >= 0 and substring[match_len] != c:
                 start_pos += shifts[match_len]
                 match_len -= shifts[match_len]
             match_len += 1
-            if match_len == len(pattern):
+            if match_len == len(substring):
                 yield start_pos
 
     def __str__(self):
